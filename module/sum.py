7f481113f804814de40570411afe9f6c79ab4830
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-01-14 14:09:57 -0500

    crea una funcion suma que recibe 2 parametros a,b

---
+++ module/sum.py
@@ -0,0 +1,13 @@
+def suma(a, b):
+    """
+    Suma dos números.
+
+    Args:
+        a (int or float): El primer número a sumar.
+        b (int or float): El segundo número a sumar.
+
+    Returns:
+        int or float: La suma de los dos números.
+    """
+    return a + b
+
