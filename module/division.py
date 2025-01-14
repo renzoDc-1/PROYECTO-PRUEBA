be9a3469aae03c8293cb3508f1ed798248d83cb5
RenzoDc-1 <renzo.delacruz1@unmsm.edu.pe>
2025-01-14 17:31:19 -0500

    agrega funcion dividir

---
+++ module/division.py
@@ -0,0 +1,12 @@
+def division(a, b):
+    """
+    multiplica dos números.
+
+    Args:
+        a (int or float): El primer número a dividir.
+        b (int or float): El segundo número a dividir.
+
+    Returns:
+        int or float: La division de los dos números.
+    """
+    return a / b
