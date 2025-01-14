a9565af2c78bf8c86f2678f16737d2f1654b26d1
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-01-14 16:33:21 -0500

    Refactorizar el código para separar la lógica de entrada y salida en funciones
    - Se creó la función `obtener_entrada` para manejar la entrada del usuario
    - Se creó la función `mostrar_resultado` para manejar la visualización de la salida
    - Se actualizó el script principal para usar las nuevas funciones para una mejor modularidad y facilidad de mantenimiento

--- index.py
+++ index.py
@@ -5,29 +5,26 @@ from module.resta import resta
 os.system("cls")
 
 def message():
-    print("Tenia que ser ")
+    print("Calculadora ")
+
+def calcular(a, b, signo):
+    if signo == '+':
+        return suma(a, b)
+    elif signo == '-':
+        return resta(a, b)
+    else:
+        return "Signo no válido"
+
+def obtener_entrada():
+    a = int(input("Ingrese el primer número: "))
+    signo = input("Ingrese el signo (+ o -): ")
+    b = int(input("Ingrese el segundo número: "))
+    return a, signo, b
+
+def mostrar_resultado(a, signo, b, resultado):
+    print(f"El resultado de {a} {signo} {b} es {resultado}")
 
 message()
-
-
-def llamar_suma():
-    a = int(input("Ingrese el primer número: "))
-    b = int(input("Ingrese el segundo número: "))
-    resultado = suma(a, b)
-    print(f"La suma de {a} y {b} es {resultado}")
-
-llamar_suma()
-
-
-def multiplicar(a, b):
-    return a * b
-
-print(multiplicar(2, 3))
-
-def llamar_resta():
-    a = int(input("Ingrese el primer número: "))
-    b = int(input("Ingrese el segundo número: "))
-    resultado = resta(a,b)
-    print(f"La resta de {a} y {b} es {resultado}")
-
-llamar_resta()
+a, signo, b = obtener_entrada()
+resultado = calcular(a, b, signo)
+mostrar_resultado(a, signo, b, resultado)
